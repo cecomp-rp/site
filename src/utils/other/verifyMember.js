9c1921b9157458a21aecd2500bb13348fb803cf5
m_iranta <lucasrezende12@hotmail.com>
2023-07-05 13:58:27 -0300

    verify bcc members

--- src/utils/other/verifyMember.js
+++ src/utils/other/verifyMember.js
@@ -1,4 +1,5 @@
-const User = require("../../database/models/User")
+const User  = require("../../database/models/User")
+const fs    = require('fs');
 
 //Verify if it is a USP member
 const verifyUspMember = async (email) => {
@@ -22,7 +23,34 @@ const verifyUspMember = async (email) => {
 
 //Verify if it is a BCC member
 const verifyBccMember = async (email) => {
-    return true
+    var isBccMember = false;
+    var lines = [];
+
+    //Read lines from document
+    const data = await fs.readFileSync('static/etc/bcc_members.txt', 'utf8');
+    lines = data.split('\n');
+
+    //Verify if email is in the list
+    lines.forEach(line => {
+        if(line == email){isBccMember = true}
+    })
+
+    //Add role to user
+    if(isBccMember){
+        const userDb = await User.findOne({email})
+        var alreadyMember = false;
+
+        userDb.roles.forEach(role => {
+            if(role == 'bcc_member'){alreadyMember = true}
+        });
+
+        if(!alreadyMember){
+            const new_roles = userDb.roles.concat('bcc_member');
+            await User.updateOne({email}, {roles: new_roles});
+        }
+    }
+
+    return isBccMember
 }
 
 module.exports = {verifyUspMember, verifyBccMember};
